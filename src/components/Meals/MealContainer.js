75340090224a4cb95918f22143c10bb3d302e161
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-04 14:52:23 -0400

    app bar working, filter working, search partially working, updating before breaking

---
+++ src/components/Meals/MealContainer.js
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+// import { MealSearch } from "./MealSearch"
+// import { MealList } from "./MealList"
+
+
+
+// export const MealContainer = () => {
+//         const [searchTerms, setSearchTerms] = useState("")
+//     return(
+//     <>
+//     <MealSearch setterFunction={setSearchTerms} example1={100} example2={"foobar"}/>
+//     <MealList searchTermState={searchTerms} /> 
+//     </>
+//     )
+//     }
